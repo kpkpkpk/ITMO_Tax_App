4d9b8c8a70e2f063dc018da485a5509c5929f91c
Puroblast <Puroblast97@mail.ru>
2024-05-15 23:15:30 +0300

    bottom nav

--- app/src/main/java/com/mobteam/taxapp/MainActivity.kt
+++ app/src/main/java/com/mobteam/taxapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mobteam.taxapp
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.mobteam.taxapp.databinding.ActivityMainBinding
 import com.mobteam.taxapp.features.auth.AuthFragment
+import com.mobteam.taxapp.features.courses.feed.ui.CoursesFragment
 import com.mobteam.taxapp.features.notes.ui.NotesFragment
 import com.mobteam.taxapp.features.profile.ProfileFragment
 import com.mobteam.taxapp.features.terms.ui.TermsFragment
@@ -14,9 +16,30 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onResume() {
         super.onResume()
+        setBottomNavigationListener()
         supportFragmentManager.commit {
-            replace(binding.container.id, NotesFragment.newInstance())
+            replace(binding.container.id, AuthFragment.newInstance())
             addToBackStack(null)
         }
     }
+
+    private fun setBottomNavigationListener() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_profile -> navigateTo(ProfileFragment.newInstance())
+                R.id.action_courses -> navigateTo(CoursesFragment.newInstance())
+                R.id.action_terms -> navigateTo(TermsFragment.newInstance())
+                R.id.action_notes -> navigateTo(NotesFragment.newInstance())
+            }
+            true
+        }
+    }
+
+    private fun navigateTo(fragment : Fragment) {
+        supportFragmentManager.commit {
+            replace(binding.container.id, fragment)
+            addToBackStack(null)
+        }
+    }
+
 }
